71fb43447653bac437c80d584a6daeb3d10510a0
carlosfiori44 <carlos.fiori@hotmail.com>
2024-06-15 18:05:14 -0300

    fix: update setup method for add admin and update gitignore to avoid sqlite files

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -1,5 +1,6 @@
 import { DataSource } from "typeorm"
 import { join } from "path"
+import UserController from "../controllers/user/user.controller"
 const dataBase = new DataSource({
     type: 'sqlite',
     database: process.env.DATABASE || './src/database/database.sqlite',
@@ -13,6 +14,7 @@ const dataBase = new DataSource({
 dataBase.initialize()
 .then(() => {
     console.log('Banco de dados iniciado!')
+    UserController.setup();
 })
 .catch((err) => {
     console.log('Falha ao inciar o banco de dados!', err)
